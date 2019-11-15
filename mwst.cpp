681b75da64a5a872cf22876fe694fc03ac1b370f
Justin Stitt <cpt.crixus@gmail.com>
2019-11-15 14:31:21 -0800

    setup

---
+++ mwst.cpp
@@ -0,0 +1,19 @@
+/*
+	Justin Stitt
+	11/15/2019
+
+	Uses Kruskal's algorithm to find the Minimum Weight Spanning Tree (MWST) of a given undirected and weighted graph.
+	also uses disjoint-set data structure.
+
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+	cin.get();
+	return 0;
+}
